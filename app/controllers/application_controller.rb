2eacad612635c74e988bdf5428018e8ea5a5b496
Jose Antonio Parra <josparbar@gmail.com>
2019-03-02 01:30:40 +0100

    Improve error message when rescue from RecordNotFound.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound do
-    render json: { message: 'The resource is not found' }, status: :not_found
+    render json: { error: {code: 404, message: 'The resource is not found' } }, status: :not_found
   end
 end
